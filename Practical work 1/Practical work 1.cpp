5ce0f8490dabb7a7cc0924745dff5ff7266f9c03
lDeathlWhisperl <62991413+lDeathlWhisperl@users.noreply.github.com>
2021-09-18 02:14:21 +0300

    Update Practical work 1.cpp

--- Practical work 1/Practical work 1.cpp
+++ Practical work 1/Practical work 1.cpp
@@ -2,9 +2,25 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "123";
+    //1
+    std::cout << "1)\n" << "int: " << sizeof(int) << "\nshort int: " << sizeof(short int) << "\nlong int: " << sizeof(long int) 
+                << "\nfloat: " << sizeof(float) << "\ndouble: " << sizeof(double) << "\nlong double: " << sizeof(long double)
+                << "\nchar:" << sizeof(char) << "\nbool: " << sizeof(bool) << "\n\n2)\n";
+
+    //2
+    int value = -255;
+    int order = 32;
+    int mask = 2147483648;
+    int j = 1;
+
+    for (int i = order; i >= 1; i--)
+    {
+
+        putchar(value & mask ? '1' : '0');
+        value <<= 1;
+
+        if (j % 8 == 0 )
+            std::cout << ' ';
+        j++;
+    }
 }
